f2fdb19cc8ab465426fd35deade941e727f3e45d
marcgalmes <marcgalmes2a@gmail.com>
2017-04-02 12:39:36 +0200

    subir ejercicios

---
+++ javaEclipse/katanumromanos/src/main/java/org/foobarspam/katanumromanos/NumerosRomanosTest.java
@@ -0,0 +1,15 @@
+package org.foobarspam.katanumromanos;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class NumerosRomanosTest extends TestCase {
+	
+	@Test
+	public void numromanoTest() {
+		//NumeroRomano numeroRomano = new NumeroRomano();
+		assertEquals(1888,1888);
+	}
+
+}
